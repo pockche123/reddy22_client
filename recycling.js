676d0998a04d95b6f0a612ad0c56a05a87b80e70
Alex Warden <alex.wardenn@gmail.com>
2023-11-28 09:23:01 +0000

    recycling now responds to answers

--- recycling.js
+++ recycling.js
@@ -1,33 +1,34 @@
-const next = document.querySelector("#create-score");
-next.addEventListener('click', createNewScore);
+// const next = document.querySelector("#create-score");
+// next.addEventListener('click', createNewScore);
 
-async function createNewScore(e) {
-    e.preventDefault()
+// async function createNewScore(e) {
+//     e.preventDefault()
 
-    const score = {
-        id: e.length + 1,
-        score: e.length + 1
-    }
+//     const score = {
+//         id: e.length + 1,
+//         score: e.length + 1
+//     }
 
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(score)
-    }
+//     const options = {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(score)
+//     }
 
-    const response = await fetch("http://localhost:3000/total", options);
+//     const response = await fetch("http://localhost:3000/total", options);
 
-    if (response.ok) {
-        // alert("Score added.");
-    }
-}
+//     if (response.ok) {
+//         // alert("Score added.");
+//     }
+// }
 
 ////////////////////////////////////////////////////////////////////////
 
 const questionContainerElement = document.getElementById('question-container')
 const backButton = document.getElementById('back-btn')
+const ItemName = document.getElementById('name')
 
 //////////////////////////////////////////////////////////////////////////
 
@@ -38,7 +39,7 @@ const nextQuestionBtnI = document.getElementById('next-btn-incorrect');
 //////////////////////////////////////////////////////////////////////////
 
 
-const outlineImg = document.getElementById('image')
+const recycleImg = document.getElementById('image')
 const body = document.getElementById('body')
 
 const button1 = document.getElementById('btn1')
@@ -49,32 +50,39 @@ const button4 = document.getElementById('btn4')
 const buttons = document.querySelectorAll('.answer')
 
 
-let score = [0]
+// let score = [0]
 
 getRandQuestion()
 
 
 function getRandQuestion() {
-    fetch("https://reddy22-server.onrender.com/")
+    const randNum = Math.floor(Math.random()*25) + 1;
+    fetch(`https://reddy-2-2-be.onrender.com/materials/${randNum}`)
      .then(res => res.json())
      .then(data => fetchInfo(data))
 }
 
 
 function fetchInfo(data){
-    outlineImg.src = data.question;
-    answers = data.answers
-    shuffledAnswers = answers.sort(() => Math.random() -0.5)
+    recycleImg.src = data.material_image;
+    bin_id = data.bin_id;
+    const name = data.name;
 
-    button1.textContent = shuffledAnswers[0].text 
-    button2.textContent = shuffledAnswers[1].text
-    button3.textContent = shuffledAnswers[2].text
-    button4.textContent = shuffledAnswers[3].text
+    const lables = [{name: "Blue", id: 1} , {name: "Grey", id: 2}, {name: "Green", id: 2}, {name: "Brown", id: 2}]
+    shuffledLables = lables.sort(() => Math.random() -0.5)
 
-    button1.bool = shuffledAnswers[0].correct
-    button2.bool = shuffledAnswers[1].correct
-    button3.bool = shuffledAnswers[2].correct
-    button4.bool = shuffledAnswers[3].correct
+    button1.textContent = shuffledLables[0].name
+    button2.textContent = shuffledLables[1].name
+    button3.textContent = shuffledLables[2].name
+    button4.textContent = shuffledLables[3].name
+
+    button1.bin = shuffledLables[0].id
+    button2.bin = shuffledLables[1].id
+    button3.bin = shuffledLables[2].id
+    button4.bin = shuffledLables[3].id
+
+
+    ItemName.textContent = name
 
     buttons.forEach(button => {
         button.addEventListener('click', checkAnswer)
@@ -83,7 +91,7 @@ function fetchInfo(data){
     backButton.addEventListener('click', () => {
         location.href = 'index.html'
     })
-    ///////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
     nextQuestionBtnC.addEventListener('click', () => {
         location.reload() // should load up a ranomdly selected question and pass current score onto it
     })
@@ -94,6 +102,7 @@ function fetchInfo(data){
 
     ///////////////////////////////////////////////////////////////////////////////
     }
+    
 fetchInfo()
 
 function checkAnswer() {
@@ -101,13 +110,14 @@ function checkAnswer() {
     button2.disabled = true;
     button3.disabled = true;
     button4.disabled = true;
-    if (this.bool === true){
+    if (this.bin === bin_id){
         this.classList.add("correct");
         question.textContent = 'Congratulations, You guessed right!'
         nextQuestionBtnC.style.display = 'block'
 
         const next = document.querySelector("#create-score");
-        next.addEventListener('click', createNewScore);
+        // next.addEventListener('click', createNewScore);
+        next.addEventListener('click');
 
     } else {
         this.classList.add("wrong");
