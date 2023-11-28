c133cea5bbc38e863ab38dcce9c49bd0bec65575
nine96as <nine96@skiff.com>
2023-11-28 15:11:41 +0000

    Add login, register, posts page skeleton

---
+++ static/js/login.js
@@ -0,0 +1,30 @@
+document.getElementById('login-form').addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const form = new FormData(e.target);
+
+  const options = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: form.get('username'),
+      password: form.get('password')
+    })
+  };
+
+  const response = await fetch(
+    'https://reddy-2-2-be.onrender.com/users/login',
+    options
+  );
+  const data = await response.json();
+
+  if (response.status === 200) {
+    localStorage.setItem('token', data.token);
+    window.location.assign('board.html');
+  } else {
+    alert(data.error);
+  }
+});
