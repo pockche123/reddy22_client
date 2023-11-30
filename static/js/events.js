3a85b72b69033a69059078afa8518a8b3e60b0a3
nine96as <nine96@skiff.com>
2023-11-30 13:34:41 +0000

    Fix issues with enroll, unenroll btns

--- static/js/events.js
+++ static/js/events.js
@@ -3,24 +3,44 @@ const createPostElement = (data) => {
   post.classList.add('container');
   post.id = 'post';
 
+  const userEnrollsContainer = document.createElement('div');
+  userEnrollsContainer.style.display = 'flex';
+  userEnrollsContainer.style.justifyContent = 'space-between';
+  post.appendChild(userEnrollsContainer);
+
   const userContainer = document.createElement('div');
   userContainer.style.display = 'flex';
   userContainer.style.gap = '12px';
   userContainer.className = 'user';
-  post.appendChild(userContainer);
+  userEnrollsContainer.appendChild(userContainer);
 
   const userIcon = document.createElement('div');
-  userIcon.id = 'icon';
   userIcon.innerHTML =
     '<svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24" style="color:var(--geist-foreground);width:24px;height:24px"><path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>';
   userContainer.appendChild(userIcon);
 
   const userName = document.createElement('p');
-  userName.id = 'name';
   userName.style.fontWeight = 'bold';
   userName.style.fontFamily = 'Inter Tight';
   fetchUsername(data['user_id']).then((name) => (userName.textContent = name));
   userContainer.appendChild(userName);
+
+  const enrollsContainer = document.createElement('div');
+  enrollsContainer.style.display = 'flex';
+  enrollsContainer.style.gap = '6px';
+  enrollsContainer.className = 'user';
+  userEnrollsContainer.appendChild(enrollsContainer);
+
+  const enrollIcon = document.createElement('div');
+  enrollIcon.innerHTML =
+    '<svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24" style="color:var(--geist-foreground);width:24px;height:24px"><path d="M9 11l3 3L22 4"/><path d="M21 12v7a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2h11"/></svg>';
+  enrollsContainer.appendChild(enrollIcon);
+
+  const enrollNum = document.createElement('p');
+  enrollNum.style.fontWeight = 'bold';
+  enrollNum.style.fontFamily = 'Inter Tight';
+  enrollNum.textContent = data['enrolls'];
+  enrollsContainer.appendChild(enrollNum);
 
   const headerContainer = document.createElement('div');
   headerContainer.style.display = 'flex';
@@ -67,11 +87,83 @@ const createPostElement = (data) => {
   content.textContent = data['content'];
   post.appendChild(content);
 
+  const dateEnrollContainer = document.createElement('div');
+  dateEnrollContainer.style.display = 'flex';
+  dateEnrollContainer.style.justifyContent = 'space-between';
+  post.appendChild(dateEnrollContainer);
+
   const date = document.createElement('p');
   date.id = 'date';
   date.textContent = moment(data['date']).fromNow();
   date.style.fontWeight = 'bold';
-  post.appendChild(date);
+  dateEnrollContainer.appendChild(date);
+
+  const btnContainer = document.createElement('div');
+  btnContainer.style.display = 'flex';
+  btnContainer.style.gap = '8px';
+  dateEnrollContainer.appendChild(btnContainer);
+
+  const enrollBtn = document.createElement('button');
+  enrollBtn.id = 'enrollBtn';
+  enrollBtn.classList.add('btn-secondary');
+  enrollBtn.textContent = 'Enroll';
+  btnContainer.appendChild(enrollBtn);
+
+  enrollBtn.addEventListener('click', async () => {
+    const options = {
+      method: 'PATCH',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        enrolls: 1
+      })
+    };
+
+    const response = await fetch(
+      `https://reddy-2-2-be.onrender.com/posts/community/${data['id']}`,
+      options
+    );
+
+    if (response.status === 200) {
+      window.location.reload();
+    } else {
+      const respData = await response.json();
+      alert(respData.error);
+    }
+  });
+
+  const unenrollBtn = document.createElement('button');
+  unenrollBtn.id = 'enrollBtn';
+  unenrollBtn.classList.add('btn-secondary');
+  unenrollBtn.textContent = 'Unenroll';
+  btnContainer.appendChild(unenrollBtn);
+
+  unenrollBtn.addEventListener('click', async () => {
+    const options = {
+      method: 'PATCH',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        enrolls: -1
+      })
+    };
+
+    const response = await fetch(
+      `https://reddy-2-2-be.onrender.com/posts/community/${data['id']}`,
+      options
+    );
+
+    if (response.status === 200) {
+      window.location.reload();
+    } else {
+      const respData = await response.json();
+      alert(respData.error);
+    }
+  });
 
   return post;
 };
@@ -91,7 +183,8 @@ document.getElementById('post-form').addEventListener('submit', async (e) => {
     body: JSON.stringify({
       title: form.get('title'),
       content: form.get('content'),
-      isCommunity: true
+      isCommunity: true,
+      enrolls: 0
     })
   };
 
